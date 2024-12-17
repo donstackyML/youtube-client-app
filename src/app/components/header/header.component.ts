96ec773d1749a4f23adf21a514d6fd2b79b402b5
Rodnykh Sergey <sergei.rodnykh@monitel.ru>
2024-12-17 16:51:54 +0300

    feat: добавлена первоначальная верстка header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PrizmInputTextModule } from '@prizm-ui/components';
+import { PrizmButtonComponent } from '@prizm-ui/components';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [PrizmInputTextModule, PrizmButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
