294272d078e3b51e478be4bcab18e5d412cb7973
Rodnykh Sergey <sergei.rodnykh@monitel.ru>
2024-12-21 14:11:11 +0300

    feat: header - добавлены иконки и выезжающий блок сортировки

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PrizmInputTextModule } from '@prizm-ui/components';
 import { PrizmButtonComponent } from '@prizm-ui/components';
-
+import { PrizmIconsFullRegistry } from '@prizm-ui/icons/core';
+import { prizmIconsFilter, prizmIconsUser } from '@prizm-ui/icons/full/source';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +12,17 @@ import { PrizmButtonComponent } from '@prizm-ui/components';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  iconsRegistry = inject(PrizmIconsFullRegistry);
+  showSortingField = false;
 
+  ngOnInit(): void {
+    this.iconsRegistry.registerIcons(prizmIconsFilter, prizmIconsUser);
+  }
+
+  toggleSortingField() {
+    console.log('toggleSortingField');
+    console.log(this.showSortingField);
+    this.showSortingField = !this.showSortingField;
+    console.log(this.showSortingField);
+  }
 }
